faf383cbf1a2f390fc314e5b92867d3dc910ac48
cleverlight <github.account@cleveraspaint.com>
2012-12-19 17:24:06 +0000

    added untested support for CentOS

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -1,6 +1,16 @@
 class dns::server::install {
 
+  case $operatingsystem {
+    centos, redhat: {
+      $package_name = 'bind'
+    }
+    ubuntu, debian: {
+      $package_name = 'bind9'
+    }
+  }
+
   package { 'bind9':
+    name => $package_name,
     ensure => latest,
   }
 
