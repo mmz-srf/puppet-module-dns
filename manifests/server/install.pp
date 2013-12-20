e098a725f7e2ab2b3e8cabb4d7d83a3482b1f5ca
Claudio Walser <claudio.walser@srf.ch>
2013-12-20 10:52:06 +0100

    Fixed dns installation with puppet 3.4.0

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -2,15 +2,13 @@ class dns::server::install {
 
   case $operatingsystem {
     centos, redhat: {
-      package { 'bind9':
-        name => ['bind','bind-utils'],
+      package { ['bind','bind-utils']:
         ensure => latest,
       }
     }
     ubuntu, debian: {
-      package { 'bind9':
-        name => ['bind9','bind9utils'],
-        ensure => latest,
+      package { ['bind9','bind9utils']:
+       ensure => latest,
       }
     }
   }
